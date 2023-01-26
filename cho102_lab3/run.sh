2a069b2631f5f58c70dfc562f60721b716076e19
Cindy Ho <cho102@ucr.edu>
2023-01-26 23:56:27 +0000

    ready for submission

--- cho102_lab3/run.sh
+++ cho102_lab3/run.sh
@@ -1,3 +1,4 @@
-3 20 5
-3 20 3,5
-3 20 3v5
+javac src/main/java/edu/ucr/cs/cs167/cho102/App.java
+java src/main/java/edu/ucr/cs/cs167/cho102/App.java 3 20 5
+java src/main/java/edu/ucr/cs/cs167/cho102/App.java 3 20 3,5
+java src/main/java/edu/ucr/cs/cs167/cho102/App.java 3 20 3v5
